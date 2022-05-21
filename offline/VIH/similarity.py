b617f60744e448304d5601000b388869bcc39d27
Ishita <ishitagops12@gmail.com>
2022-05-21 14:29:37 +0530

    test function to perform similarity testing

--- offline/VIH/similarity.py
+++ offline/VIH/similarity.py
@@ -19,6 +19,18 @@ def check_similarity(cnt_1, cnt_2):
     print(main_doc.similarity(search_doc))
 
 
+def test():
+
+    cnt_1 = '''
+    Amazon Elastic Compute Cloud (Amazon EC2) is a web service that provides secure, resizable compute capacity in the cloud.
+    '''
+
+    cnt_2 = '''
+    Amazon Elastic Compute Cloud (Amazon EC2) provides scalable computing capacity in the Amazon Web Services (AWS) Cloud. Using Amazon EC2 eliminates your need to invest in hardware up front, so you can develop and deploy applications faster.
+    '''
+    
+    check_similarity(cnt_1, cnt_2)
+    
 if __name__ == '__main__':
 
-    check_similarity()
+    test()
