19745a30dca01fc9be97284b8faa6daf990a4ded
ganeshcaot <105956128+ganeshcaot@users.noreply.github.com>
2022-05-21 12:51:47 +0530

    Create main.py

---
+++ offline/CAOT/main.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+import imutils
+from time import perf_counter
+
+
+cap = cv2.VideoCapture("in.avi")
+frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+fourcc = cv2.VideoWriter_fourcc('X','V','I','D')
+out = cv2.VideoWriter("output.avi", fourcc, 10, (1280,720))
+
+HOGCV = cv2.HOGDescriptor()
+HOGCV.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+
+last_move=perf_counter()
+
+
+def person_detect(frame):
+    bounding_box_cordinates, weights = HOGCV.detectMultiScale(frame, winStride = (4, 4), padding = (8, 8), scale = 1.02)
+    person = 1
+    for x, y, w, h in bounding_box_cordinates:
+        cv2.rectangle(frame, (x,y), (x+w,y+h), (0,0,255), 2)
+        cv2.putText(frame, f'person {person}', (x,y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 1)
+        person += 1
