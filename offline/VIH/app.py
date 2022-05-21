d452a04d3feb2f0e4dbaca1f9e989f06de3106e7
VedhaSankar <vedhasankar26@gmail.com>
2022-05-21 16:16:12 +0530

    image detection

--- offline/VIH/app.py
+++ offline/VIH/app.py
@@ -2,12 +2,19 @@ from flask import Flask,request, flash, redirect, render_template, request
 import pickle
 from score import predict_link_score as detect_link
 from score import predict_content_score as detect_content
-
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 from dotenv import load_dotenv
+from email.mime.application import MIMEApplication
+import os
+from flask_mail import Mail, Message
+from emailer import send_email
+from werkzeug.utils import secure_filename
+from text_extraction import image_to_string
 
-
-app     = Flask(__name__)
-mail    = Mail(app)
+app = Flask(__name__)
+mail= Mail(app)
 
 # https://www.dnaindia.com/education/report-cbse-class-10-12-board-exam-2022-term-1-cancellation-latest-updates-news-exam-centre-datesheetonline-petition-2917207
 # https://www.bbc.com/news/live/world-europe-60685883
@@ -34,45 +41,67 @@ app.config['MAIL_USE_SSL'] = True
 app.config["UPLOAD_FOLDER"] = "uploads/"
 mail = Mail(app)
 
-ALLOWED_EXTENSIONS  = {'png', 'jpg', 'jpeg'}
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
 
 
 @app.route('/', methods = ["GET","POST"])
 def home():
 
     if request.method == "POST":
-
         try:
-
             content = request.values.get("news_content")
             prediction = detect_content(content)
-
         except:
-
             pass
-
         try:
-
             link = request.values.get("news_link")
             prediction = detect_link(link)
             print("Prediction: ", prediction)        
 
         except:
-
             pass
-
         return render_template("index.html", prediction = prediction)
 
     return render_template("index.html")
 
-@app.route('/ping')
-def ping():
+@app.route('/alert', methods = ["GET", "POST"])
+def alert():
 
-    result = {
-        "ping"  : "pong"
-    }
-    return result
+    for sender in EMAIL_LIST:
+        send_email(
+            receiver_address=sender,
+            subject='Alert',
+            content="The mentioned content is FAKE, alerted the authorities!!"
+            )
 
-if __name__== "__main__":
+    return render_template("index.html")
 
-    app.run(host="0.0.0.0", debug = True, port = 5003)
+@app.route('/image-detection', methods = ["GET", "POST"])
+def image_detection():
+
+    if request.method == 'POST':
+
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+
+        file = request.files['file']
+
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+
+    return render_template('image-detection.html')
+
+
+if __name__ == "__main__":
+
+    app.run(debug = True,host="0.0.0.0", port = 8500)
+
+
+
+
+
+
+
