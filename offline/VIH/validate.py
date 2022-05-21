2feb411b445a7d5b762ded58a1d8190a3c6f6898
Harene M <harenemaharajan02@gmail.com>
2022-05-21 14:59:59 +0530

    added two functions

--- offline/VIH/validate.py
+++ offline/VIH/validate.py
@@ -8,7 +8,6 @@ def wordopt(text):
 
     ps = PorterStemmer()
 
-
     text = re.sub('[^a-zA-Z]', ' ',text)
 
     text = text.lower()
@@ -20,3 +19,42 @@ def wordopt(text):
     text = ' '.join(text)
 
     return text
+
+def load_model():
+    
+    with open('model_svm.pkl','rb') as f:
+
+        model = pickle.load(f)
+
+    with open('vectorizer.pkl','rb') as f:
+
+        vectorizer = pickle.load(f)
+    
+    return (model, vectorizer)
+
+
+def fake_news_det(news):
+
+    svm_model, vectorizer = load_model()
+
+    input_data = {
+        "text": [news]
+    }
+
+    new_def_test = pd.DataFrame(input_data)
+
+    new_def_test["text"] = new_def_test["text"].apply(wordopt) 
+
+    new_x_test = new_def_test["text"]
+
+    # print(type(new_x_test))
+
+    vectorized_input_data = vectorizer.transform(new_x_test)
+
+    prediction = svm_model.predict(vectorized_input_data)
+    
+    if prediction == 1:
+        return 1
+    else:
+        return 0
+
